3e830ed41bb7d8dcf0dbd35dfc58f5adca30eef3
Vicky Roberts <vicki_uk@hotmail.com>
2017-06-27 15:26:59 +0100

    User cookie added on signup

--- apps/rsvp_web/web/controllers/signup_controller.ex
+++ apps/rsvp_web/web/controllers/signup_controller.ex
@@ -14,7 +14,14 @@ defmodule RsvpWeb.SignupController do
     IO.puts "these are the user params from the signup form"
     IO.inspect users
     changeset = Rsvp.Users.changeset(%Rsvp.Users{}, users)
+    expiration = 60
+  conn
+    |> Plug.Conn.put_resp_cookie("user_name", users["username"], max_age: expiration)
+    |> signup_completion(changeset)
+  end
 
+  def signup_completion(conn, changeset) do
+    IO.inspect conn
     case Rsvp.UserQueries.create changeset do
       {:ok, %{id: id}} -> redirect conn, to: ("/")
       {:error, reasons} -> index conn, %{errors: reasons}
